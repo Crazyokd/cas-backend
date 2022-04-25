bee626e71e9616003910b9d233763adf4dd4f0e4
Rekord <1324596506@qq.com>
2022-04-25 20:02:41 +0800

    feat: add exception/

---
+++ src/main/java/oct/rekord/cas/exception/BaseException.java
@@ -0,0 +1,16 @@
+package oct.rekord.cas.exception;
+
+import lombok.Getter;
+
+
+@Getter
+public class BaseException extends RuntimeException{
+    private Integer code;
+    private String msg;
+
+
+    public BaseException(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
