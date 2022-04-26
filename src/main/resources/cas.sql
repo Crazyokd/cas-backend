fcde5f7894a471be6c9dea5128784e6609e43714
Rekord <1324596506@qq.com>
2022-04-26 23:32:28 +0800

    refactor: refactor the design of database

--- src/main/resources/cas.sql
+++ src/main/resources/cas.sql
@@ -20,6 +20,11 @@ create table if not exists user
         unique index `username`(`username`) using Btree
 	) ENGINE=InnoDB auto_increment=125 DEFAULT charset=utf8;
 
+-- 设定超级管理员 root
+insert into user(`username`, `password`, `phone`, `level`) values("root", "123456", "18273447586", '0');
+-- 删除账号
+-- delete from user where `username` = "root";
+
 
 drop table if exists manager_1;
 -- 创建manager_1表
@@ -29,8 +34,12 @@ create table if not exists manager_1
         `user_id` int(11) not null comment '用户ID',
 		primary key(`manager_id`) using BTREE,
         constraint manager1_user_id foreign key(`user_id`) references user(`user_id`)
-    );
+    ) engine=InnoDB auto_increment=125 default charset=utf8;
     
+-- 设立一级管理员并插入到 manager_1 表中
+insert into user(`username`, `password`, `phone`, `level`) values("manager1", "123456", "18273447586", "1");
+insert into manager_1(`user_id`) values(126);
+
 
 drop table if exists manager_2;
 -- 创建manager_2表
@@ -42,9 +51,26 @@ create table if not exists manager_2
         primary key(`manager_id`) using BTREE,
         constraint manager2_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade,
 		constraint manager2_parent foreign key(`parent`) references manager_1(`manager_id`) on delete cascade
-    );
-      
-      
+    ) engine=InnoDB auto_increment=125 default charset=utf8;
+
+-- 设立二级管理员并插入到 manager_2 表中 并保证与 manager_1 中的管理员建立关联      
+insert into user(`username`, `password`, `phone`, `level`) values('manager_2', "123456", "18273447586", "2");
+insert into manager_2(`user_id`, `parent`) values(127, 125);
+
+-- 注册普通用户
+insert into user(`user_id`, `username`, `password`, `phone`) values(150, "rekord", "011010", "19330381990");
+insert into user(`user_id`, `username`, `password`, `phone`) values(151, "test1", "123456", "15253238439");
+insert into user(`user_id`, `username`, `password`, `phone`) values(152, "test2", "123456", "15298728239");
+-- 绑定学工号
+update user 
+set sn = "201903130218", sn_password = "17573401869@yiCAO"
+where username = "rekord";
+-- 绑定邮箱
+update user
+set email = "wildrekord@gmail.com"
+where username = "rekord";
+
+
 -- drop table if exists head_img;
 -- -- 创建head_img表
 -- CREATE TABLE if not exists `head_img`  (
@@ -57,6 +83,21 @@ create table if not exists manager_2
 -- ) ENGINE = InnoDB AUTO_INCREMENT = 265 default charset=utf8;
 
 
+
+
+drop table if exists semester;
+-- 创建学期表
+create table if not exists semester 
+	(
+		`semester_id` int(11) not null auto_increment comment '学期ID',
+        `semester_name` varchar(255) not null comment '学期',
+        primary key(`semester_id`) using btree,
+        unique index (`semester_name`) using btree
+    ) engine=InnoDB auto_increment=100 default charset=utf8;
+
+-- 建立 2021-2022 学年 
+insert into semester(`semester_name`) values('2021-2022');   
+    
 drop table if exists activity;
 -- 创建活动表
 create table if not exists activity 
@@ -72,9 +113,75 @@ create table if not exists activity
         act_time datetime(0) comment '活动时间',
         act_place varchar(255) comment '活动地点',
         act_category enum('劳育','体育','德育','智育') default '德育' comment '活动类别',
+        `semester_id` int(11) not null comment '所属学期',
         primary key(`act_id`),
-        unique index (`act_name`) using BTREE
+        unique index (`act_name`) using BTREE,
+        constraint activity_semester_id foreign key(`semester_id`) references semester(`semester_id`) 
 	) ENGINE=InnoDB auto_increment=500 DEFAULT charset=utf8;
+
+-- 删除活动
+-- delete from activity 
+-- where `act_id` = "500";
+    
+
+drop table if exists par_activity;
+-- 创建参与活动表
+create table if not exists par_activity 
+	(
+		`id` int(11) not null auto_increment comment '参与活动ID',
+        `user_id` INT(11) not null comment '用户ID',
+        `act_id` int(11) not null comment '活动ID',
+        `reg_number` int not null comment '报名号',
+        `grade` int comment '活动所获分数',
+        `explanation` varchar(255) not null default "普通参与" comment '加分说明',
+        primary key(`id`),
+        constraint par_activity_act_id foreign key(`act_id`) references activity(`act_id`) on delete cascade,
+        constraint par_activiry_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade
+	) ENGINE=InnoDB DEFAULT charset=utf8;
+    
+    
+drop table if exists man_activity;
+-- 创建管理活动表
+create table if not exists man_activity 
+	(
+		`id` int(11) not null auto_increment comment '管理活动ID',
+        `user_id` int(11) not null comment '用户ID',
+        `act_id` int(11) not null comment '活动ID',
+        primary key(`id`),
+        constraint man_activity_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade,
+        constraint man_activity_act_id foreign key(`act_id`) references activity(`act_id`) on delete cascade
+	) ENGINE=InnoDB DEFAULT charset=utf8;
+    
+-- 发布活动并分别指定发布者
+insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`, `semester_id`)
+values ("计通学院凌云杯第一场篮球赛观众招募", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行凌云杯篮球比赛", 
+	80, '2022-02-27', '2022-02-28', '2022-03-02 15:00', "云塘校区体育馆二楼", "德育", 100);
+insert into man_activity(`user_id`, `act_id`) values('126', '500');
+insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`, `semester_id`)
+values ("“互联网+”创新大赛院赛报名", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
+	100, '2022-03-28', '2022-06-28', '2022-06-30 15:00', "", "德育", 100);
+insert into man_activity(`user_id`, `act_id`) values('126', '501');
+insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`, `semester_id`)
+values ("测试活动1", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
+	100, '2022-06-24', '2022-06-28', '2022-06-29 15:00', "", "德育", 100);
+insert into man_activity(`user_id`, `act_id`) values('127', '502');
+insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`, `semester_id`)
+values ("测试活动2", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
+	100, '2022-03-22', '2022-05-23', '2022-04-24 15:00', "", "德育", 100);      
+insert into man_activity(`user_id`, `act_id`) values('127', '503');
+
+-- 用户报名活动
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(126, 500, 1);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(126, 502, 1);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(127, 501, 1);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(150, 500, 2);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(150, 501, 2);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(150, 502, 2);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(150, 503, 1);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(151, 500, 3);
+insert into par_activity(`user_id`, `act_id`, `reg_number`) values(152, 501, 3);
+
+
 
 
 drop table if exists award_certificate;
@@ -82,18 +189,29 @@ drop table if exists award_certificate;
 create table if not exists award_certificate
 	(
 		`ac_id` int(11) not null auto_increment comment '获奖证书id',
-        `user_id` int(11) not null comment '用户ID',
         `name` varchar(255) not null comment'证书名',
+        `user_id` int(11) not null comment '用户ID',
         `is_valid` enum('0', '1') default '0' comment '是否纳入综测',
-        `grade` int comment '加分',
         `category` enum('劳育','体育','德育','智育') default '德育' comment '证书类别', 
+        `grade` int comment '加分',
         `explanation` varchar(80) not null comment '加分说明',
-        `comment` varchar(255) comment '证书备注',
-        `img_path` varchar(255) default '/home/rekord/cas/img/ACImg/default/default.jpg' comment '图片路径',
+        `comment` varchar(255) not null default "" comment '证书备注',
+        `img_path` varchar(255) default '/home/rekord/cas/img/acImg/default/default.jpg' comment '图片路径',
         `update_date` datetime DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP(0) comment '更新时间',
+        `semester_id` int(11) not null,
         primary key(`ac_id`),
-        constraint ac_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade
+        unique index ac_name(`name`) using btree,
+        constraint ac_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade,
+        constraint ac_semester_id foreign key(`semester_id`) references semester(`semester_id`)
 	) ENGINE=InnoDB DEFAULT charset=utf8;
+
+-- 上传证书
+insert into award_certificate(`user_id`, `name`, `is_valid`, `category`, `explanation`, `comment`, `img_path`, `semester_id`)
+values(150, "第十三届程序设计大赛", "0", "德育", "校级一等奖", "", "/home/rekord/cas/img/ACImg/cxsjdsddd.jpg", 100);
+insert into award_certificate(`user_id`, `name`, `is_valid`, `category`, `explanation`, `comment`, `semester_id`)
+values(150, "第十四届程序设计大赛", "0", "德育", "校级一等奖", "", 100);
+insert into award_certificate(`user_id`, `name`, `is_valid`, `category`, `explanation`, `semester_id`)
+values(150, "第十五届程序设计大赛", "0", "德育", "校级一等奖", 100);
 
 
 drop table if exists message;
@@ -132,14 +250,6 @@ create table if not exists application
 	) ENGINE=InnoDB auto_increment=500 default charset=utf8;
     
 
--- 查询所有发起的申请
-select * from application 
-where application_from_id = 125;
--- 查询所有收到的申请
-select * from application
-where application_to_id = 125;
--- 查看申请内容
-
 DROP TABLE IF EXISTS `version`;
 -- 创建版本表
 CREATE TABLE if not exists `version`  (
@@ -154,58 +264,6 @@ CREATE TABLE if not exists `version`  (
 
 
 
-drop table if exists par_activity;
--- 创建参与活动表
-create table if not exists par_activity 
-	(
-		`id` int(11) not null auto_increment comment '参与活动ID',
-        `user_id` INT(11) not null comment '用户ID',
-        `act_id` int(11) not null comment '活动ID',
-        `reg_number` int not null comment '报名号',
-        `grade` int comment '活动所获分数',
-        `explanation` varchar(255) not null comment '加分说明',
-        primary key(`id`),
-        constraint par_activity_act_id foreign key(`act_id`) references activity(`act_id`) on delete cascade,
-        constraint par_activiry_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade
-	) ENGINE=InnoDB DEFAULT charset=utf8;
-    
-    
-drop table if exists man_activity;
--- 创建管理活动表
-create table if not exists man_activity 
-	(
-		`id` int(11) not null auto_increment comment '管理活动ID',
-        `user_id` int(11) not null comment '用户ID',
-        `act_id` int(11) not null comment '活动ID',
-        primary key(`id`),
-        constraint man_activity_user_id foreign key(`user_id`) references user(`user_id`) on delete cascade,
-        constraint man_activity_act_id foreign key(`act_id`) references activity(`act_id`) on delete cascade
-	) ENGINE=InnoDB DEFAULT charset=utf8;
-
-    
-
-
-
-
-
-
-
--- 设定超级管理员
-insert into user(`username`, `password`, `phone`, `level`) values("root", "123456", "18273447586", '0');
--- 设定一级管理员
-insert into user(`username`, `password`, `phone`, `level`) values("manager1", "123456", "18273447586", "1");
-insert into manager_1(`user_id`) values(126);
--- 注册
-insert into user(`username`, `password`, `phone`) values("rekord", "011010", "19330381990");
--- 绑定学工号
-update user 
-set sn = "201903130218", sn_password = "17573401869@yiCAO"
-where username = "rekord";
--- 绑定邮箱
-update user
-set email = "wildrekord@gmail.com"
-where username = "rekord";
-
      
 -- 查询所有用户
 select * from user;
@@ -216,99 +274,41 @@ select * from user where sn = "201903130218" and sn_password = "17573401869@yiCA
 -- 根据手机号登录
 select * from user where phone = "19330381990";
 
--- 删除账号
--- delete from user where `username` = "root";
 
-        
-        
--- 删除活动
-delete from activity 
-where `act_id` = "500";
-
--- 发布活动
-insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`)
-values ("计通学院凌云杯第一场篮球赛观众招募", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行凌云杯篮球比赛", 
-	80, '2022-02-27', '2022-02-28', '2022-03-02 15:00', "云塘校区体育馆二楼", "德育");
-insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`)
-values ("“互联网+”创新大赛院赛报名", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
-	100, '2022-02-24', '2022-02-28', '2022-03-02 15:00', "", "德育");
-insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`)
-values ("测试活动2", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
-	100, '2022-02-24', '2022-04-28', '2022-04-29 15:00', "", "德育");
-insert into activity(`act_name`, `act_description`, `act_reg_max_count`, `act_reg_start_date`, `act_reg_end_date`, `act_time`, `act_place`, `act_category`)
-values ("测试活动3", "为了丰富学生的课余生活，提高学生的综合素质，培养团队精神，增强集体凝聚力，展现学生积极向上的风貌，2022年3月，计通学院将举行“互联网+”创新大赛院赛报名", 
-	100, '2022-04-22', '2022-04-23', '2022-04-24 15:00', "", "德育");    
-
--- 全部查询
+-- 查询所有已发布的活动
 select * from activity;
 -- 分页查询所有正在进行报名的活动 
 select * from activity
 where datediff(current_date, act_reg_start_date) >= 0
 order by `act_reg_start_date` desc
 limit 0,4;
-
--- 查询所有已经结束（并非报名结束）的活动
+-- 分页查询所有已经结束（并非报名结束）的活动
 select * from activity
-where datediff(current_timestamp, act_time) < 0
+where datediff(current_timestamp, act_time) > 0
 order by `act_reg_end_date` desc
 limit 0,4;
-
-
--- 如果结果大于0,则不执行插入语句
-select count(`id`) from par_activity
-where par_activity.user_id = 125 and par_activity.act_id = 500;
--- 报名活动
-insert into par_activity(`user_id`, `act_id`, `reg_number`, `explanation`)
-values(125, 503, 1, "普通参与");
-
--- 查询所有已报名的活动
+-- 查询 rekord 所有已报名的活动
 select activity.* from par_activity, activity
-where par_activity.user_id = 125 and par_activity.act_id = activity.act_id
+where par_activity.user_id = 150 and par_activity.act_id = activity.act_id
 order by `act_reg_start_date` desc
 limit 0,4;
 
 
--- 上传证书
-insert into award_certificate(`user_id`, `name`, `is_valid`, `explanation`)
-values("125", "程序设计大赛", "1", "校级二等奖");
-
--- 查询所有证书
-select * from award_certificate;
-
--- 查询特定用户下的所有证书
-select award_certificate.* from `award_certificate`, `user`
-where user.`user_id` = 125 and user.user_id = award_certificate.user_id;
-
-
--- 查询par_activity
-select * from par_activity;
-
 -- 查询特定用户的综测细则 （活动）
-select activity.act_name, par_activity.grade, activity.act_category, par_activity.explanation 
+select activity.act_name as `name`, par_activity.grade as `grade`, activity.act_category as `category`, par_activity.explanation as `explanation` 
 from `par_activity`, `user`, `activity`
 where user.username = "rekord" and user.user_id = par_activity.user_id and par_activity.act_id = activity.act_id;
 -- 证书纳入
-select award_certificate.name, award_certificate.grade, award_certificate.category, award_certificate.explanation from `award_certificate`, `user`
-where user.`user_id` = 125 and user.user_id = award_certificate.user_id and
+select award_certificate.name as `name`, award_certificate.grade as `grade`, award_certificate.category as `category`, award_certificate.explanation as `explanation`
+from `award_certificate`, `user`
+where user.`user_id` = 150 and user.user_id = award_certificate.user_id and
 is_valid = '1';
-
 
 -- 查询今日活动提醒
 select * from par_activity, activity
-where `user_id` = 125 and par_activity.act_id = activity.act_id and
+where `user_id` = 150 and par_activity.act_id = activity.act_id and
 datediff(current_date(), activity.act_time) = 0; 
 
-
--- 查询特定用户下的所有证书
-select award_certificate.* from `award_certificate`, `user`
-where user.`username` = "rekord" and user.user_id = award_certificate.user_id and award_certificate.user_id;
- 
--- 成功发布活动
-insert into man_activity(`user_id`, `act_id`)
-values(125, 500);
--- 查询特定用户管理的所有活动
-select man_activity.* from `man_activity`
-where user_id = 125;
 
 
 
