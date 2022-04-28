5ca8d09dabc0bbf92895a99a10ae1605e7b0bc3b
Rekord <1324596506@qq.com>
2022-04-28 21:33:09 +0800

    refactor: refactor the interfaces related to activity

--- src/main/java/oct/rekord/cas/bean/Activity.java
+++ src/main/java/oct/rekord/cas/bean/Activity.java
@@ -1,6 +1,10 @@
 package oct.rekord.cas.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 public class Activity {
@@ -31,6 +35,25 @@ public class Activity {
     // 活动类别
     private String actCategory;
 
+    // 所属学期ID
     private Integer actSemesterId;
 
+    // 活动打卡开始时间
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date actPunchStartTime;
+
+    // 活动打卡结束时间
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date actPunchEndTime;
+
+    // 活动打卡经度
+    private Double actPunchLongitude;
+
+    // 活动打卡纬度
+    private Double actPunchLatitude;
+
+    // 活动是否成功发布
+    private String actIsPublish;
 }
