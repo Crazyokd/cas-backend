13eb504f91d47a7823447a41e2234905a7de3c0f
Rekord <1324596506@qq.com>
2022-05-19 18:41:31 +0800

    feat: add the function of chat-together

--- src/main/java/oct/rekord/cas/websocket/WebSocketHandler.java
+++ src/main/java/oct/rekord/cas/websocket/WebSocketHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -36,6 +37,30 @@ public class WebSocketHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         System.out.println(session.getAttributes().get("userId") + "发来消息：" + message.getPayload());
+//        // 打印 id
+//        System.out.println(session.getId());
+//        // 打印接受的协议
+//        System.out.println(session.getAcceptedProtocol());
+//        // 打印 session 是否打开
+//        System.out.println(session.isOpen());
+//        // 打印 二进制消息大小限制
+//        System.out.println(session.getBinaryMessageSizeLimit());
+//        // 打印扩展
+//        System.out.println(session.getExtensions());
+//        // 打印握手头部
+//        System.out.println(session.getHandshakeHeaders());
+//        // 打印本地地址
+//        System.out.println(session.getLocalAddress());
+//        // 打印原则
+//        System.out.println(session.getPrincipal());
+//        // 打印远程地址
+//        System.out.println(session.getRemoteAddress());
+//        // 打印文本消息大小限制
+//        System.out.println(session.getTextMessageSizeLimit());
+//        // 打印 uri
+//        System.out.println(session.getUri());
+
+        sendString(session, message.getPayload());
     }
 
     /**
@@ -55,6 +80,18 @@ public class WebSocketHandler extends TextWebSocketHandler {
     }
 
     /**
+     * 发生异常，关闭连接
+     * @param session
+     * @param exception
+     * @throws Exception
+     */
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        SESSION_POOLS.remove(session.getAttributes().get("userId"));
+        log.info("websocket发生异常！" ,exception);
+    }
+
+    /**
      * 连接断开后调用此方法
      *
      * @param session
@@ -66,6 +103,21 @@ public class WebSocketHandler extends TextWebSocketHandler {
         onlineNum.decrementAndGet();
         Map<String, Object> attributes = session.getAttributes();
         Integer userId = Integer.valueOf((String) attributes.get("userId"));
+        log.info("用户" + userId + "断开连接");
         SESSION_POOLS.remove(userId);
+    }
+
+    /**
+     * 发送消息
+     * @param message
+     * @throws IOException
+     */
+    public static void sendString(WebSocketSession fromSession, String message) throws IOException {
+        for (WebSocketSession webSocket : SESSION_POOLS.values()) {
+            if (webSocket.isOpen() && webSocket != fromSession) {
+                webSocket.sendMessage(new TextMessage(message));
+            }
+        }
+        log.debug("webSocket发送消息，内容：{}，当前连接数：{}", message, SESSION_POOLS.size());
     }
 }
